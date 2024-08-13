c5b06d0e908770dca094add86861663b69ee0487
yuris2024 <85042316+yuris2024@users.noreply.github.com>
2024-08-13 12:12:37 -0300

    Add gridcontainer that populates itself with cells

---
+++ README.md
@@ -0,0 +1,4 @@
+** Minesweeper clone in Godot **
+
+This is an attempt to clone Minesweeper using Godot Engine.
+This project is supposed to be expanded upon later.
---
+++ cell.gd
@@ -0,0 +1,15 @@
+extends Button
+
+var adjacent_mines: int = 0
+var is_mine: bool = false
+var is_open: bool = false
+var is_flagged: bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
