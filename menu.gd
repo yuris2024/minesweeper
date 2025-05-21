99fc7c723b3321fa3868ca0c504a55419a50db8b
yuris2024 <85042316+yuris2024@users.noreply.github.com>
2025-05-21 00:53:24 -0300

    Create new game functionality
    New game button now overwrites old save with a new, empty one.

--- menu.gd
+++ menu.gd
@@ -6,24 +6,25 @@ var save_file_name = "save.tres"
 #
 var save_data = SaveData.new()
 
-
 func _on_continua_pressed() -> void:
-	# read saved data...
+	# Carrega o jogo salvo.
 	save_data = ResourceLoader.load(save_file_path + save_file_name).duplicate(true)
-	print(str(save_data.coins))
-	# feed to gamemanager...
-	
 	get_tree().change_scene_to_file('res://base_scripts/GameManager.tscn')
-
-#func _on_save_pressed() -> void:
-	##save_data.bg = 
-	#ResourceSaver.save(save_data, save_file_path + save_file_name)
-	#print("saved on " + save_file_path + save_file_name)
-
 #endregion
 
 func _on_novo_jogo_pressed() -> void:
+	# Confirmar se é pra deletar o save antigo
+	$ConfirmNewGame.visible = true
+
+func _on_confirm_new_game_confirmed() -> void:
+	# Trocar o save pelo save padrão
+	save_data = ResourceLoader.load('res://save/default_save.tres').duplicate(true)
+	ResourceSaver.save(save_data, save_file_path + save_file_name)
+	# E continuar o jogo a partir desse save novo
 	get_tree().change_scene_to_file('res://base_scripts/GameManager.tscn')
+
+func _on_confirm_new_game_canceled() -> void:
+	$ConfirmNewGame.visible = false
 
 func _on_como_jogar_pressed() -> void:
 	get_tree().change_scene_to_file('res://HowToPlay.tscn')
@@ -35,3 +36,8 @@ func _on_recordes_pressed():
 	$RecordsPanel.advanced = save_data.records[2]
 	$RecordsPanel.update_label()
 	$RecordsPanel.visible = true
+
+#func _on_save_pressed() -> void:
+	##save_data.bg = 
+	#ResourceSaver.save(save_data, save_file_path + save_file_name)
+	#print("saved on " + save_file_path + save_file_name)
--- save/default_save.gd
+++ save/default_save.gd
@@ -10,25 +10,25 @@ class_name SaveData
 @export var flag = "res://inventory/items/Flags/flag_red.tres"
 @export var mine = "res://inventory/items/Mines/mine_red.tres"
 
-func set_coins(x):
-	coins = x
-
-func set_experience(x):
-	experience = x
-
-func set_inventory(x):
-	inventory = x
-
-func set_appearance(x,y,z):
-	bg = x
-	flag = y
-	mine = z
-
-func set_bg(x):
-	bg = x
-
-func set_flag(x):
-	flag = x
-	
-func set_mine(x):
-	mine = x
+#func set_coins(x):
+	#coins = x
+#
+#func set_experience(x):
+	#experience = x
+#
+#func set_inventory(x):
+	#inventory = x
+#
+#func set_appearance(x,y,z):
+	#bg = x
+	#flag = y
+	#mine = z
+#
+#func set_bg(x):
+	#bg = x
+#
+#func set_flag(x):
+	#flag = x
+	#
+#func set_mine(x):
+	#mine = x
